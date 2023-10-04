a014cf5ca61df6c125ff179952d3b4578b835c26
Ericdid <ericdidom@gmail.com>
2023-10-04 05:32:54 +0200

    add.v-for

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const { createApp } = Vue;
 
 createApp({
+  data() {
+    return {
+      disksList: [],
+    };
+  },
+
   mounted() {
-    console.log(axios);
+    // console.log(axios);
+    axios
+      .get("http://localhost/php-dischi-json/data/dischi.php")
+      .then((response) => {
+        // console.log(res);
+        this.disksList = response.data;
+        // console.log(disksList);
+      });
   },
 }).mount("#app");
